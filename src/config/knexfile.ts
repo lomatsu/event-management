520cb79283cf1c6b1cd7a718dcbd639f64ca332c
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-27 15:22:44 -0300

    add knexfile

---
+++ src/config/knexfile.ts
@@ -0,0 +1,77 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: "../../.env" })
+
+import { DB_HOST, POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER, DB_PORT } from '.'
+
+
+const connection = {
+  host: "localhost",
+  user: "postgres",
+  database: "events_management_db",
+  port: 5433,
+  password: "1234@mudar",
+}
+
+module.exports = { 
+  development: {
+    client: 'pg',
+    connection,
+    migrations: {
+      directory: "../database/migrations",
+    },
+    seeds: {
+      directory: "../database/seeds",
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      port: DB_PORT,
+      host: DB_HOST,
+      database: POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    },
+    migrations: {
+      directory: "../database/migrations",
+    },
+    seeds: {
+      directory: "../database/seeds",
+    },
+  },
+
+  test: {
+    client: "pg",
+    connection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "../database/migrations",
+    },
+    seeds: {
+      directory: "../database/seeds",
+    },
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+};
