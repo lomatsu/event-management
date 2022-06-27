3fc06134df1c563de9d1039192ebc15e68643441
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-27 15:11:20 -0300

    add ControllerBase

---
+++ src/common/ControllerBase.ts
@@ -0,0 +1,16 @@
+import { Application, Request, Response } from "express"
+
+export interface IControllerBase {
+	getAll(req: Request, res: Response): void
+  getByName(req: Request, res: Response): void
+	save(req: Request, res: Response): void
+	registerRoutes(): void
+}
+
+export abstract class ControllerBase<R> implements IControllerBase {
+	constructor(protected app: Application, protected repository: R) {}
+	public abstract getAll(req: Request, res: Response): void
+  public abstract getByName(req: Request, res: Response): void
+	public abstract save(req: Request, res: Response): void
+	public abstract registerRoutes(): void
+}
