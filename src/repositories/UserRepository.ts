2a8dd3f0087ed2a37d606fe7793c0fdb117ef140
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-28 09:33:10 -0300

    add Repository, UserRepository and configs

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,41 @@
+import { Knex } from "knex";
+import { tableName } from "../database/helps";
+import { UserModel } from "../database/model/User";
+import { IRepository } from "./Repository";
+
+export interface IUserRepository extends IRepository<UserModel> {
+
+}
+
+export class UserRepository implements IUserRepository {
+	constructor(private db: Knex) { }
+	getAll(): Promise<UserModel[]> {
+		return this.db.select("*").from(tableName.USERS)
+	}
+	getById(id: number): Promise<UserModel> {
+		return this.db.select("*").from(tableName.USERS).where({ id }).then((d) => d[0])
+	}
+	create(data: UserModel): Promise<UserModel> {
+		return this.db.insert(data, "*").into(tableName.USERS).then((d) => d[0])
+	}
+	async update(data: Partial<UserModel>): Promise<UserModel> {
+		const model = await this.getById(data.id!)
+		if (!model) {
+			throw new Error("User not found")
+		}
+
+		Object.keys(data).forEach((key: string) => {
+			if ((data as any)[key]) {
+				; (model as any)[key] = (data as any)[key]
+			}
+		})
+		return this.db.update(data, "*").where({ id: data.id }).into(tableName.USERS).then((d) => d[0])
+	}
+	async delete(id: number): Promise<boolean> {
+		const model = await this.getById(id)
+		if (!model) {
+			throw new Error("User not found")
+		}
+		return this.db.where(id).from(tableName.USERS).del()
+	}
+}
