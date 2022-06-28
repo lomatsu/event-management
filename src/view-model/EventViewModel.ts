5059cbcfb35b80e8b64fa290f90bb968499d17d0
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-28 17:41:44 -0300

    add place capacity on event view model

--- src/view-model/EventViewModel.ts
+++ src/view-model/EventViewModel.ts
@@ -1,4 +1,5 @@
 import { EventModel } from "../database/model/Event"
+import { PlaceModel } from "../database/model/Place"
 
 export class EventViewModel {
   id: number
@@ -6,15 +7,17 @@ export class EventViewModel {
   description: string
   date: string
   ticketsSold: number
+	placeCapacity?: number
   placeId: number
   companyId: number
 
-  constructor(params: EventModel) {
+  constructor(params: EventModel, places?: PlaceModel[]) {
     this.id = params.id
     this.name = params.name
     this.description = params.description
     this.date = params.date
     this.ticketsSold = params.tickets_sold
+		this.placeCapacity = places?.find((x) => x.id === params.place_id)?.total_amount
     this.placeId = params.place_id
     this.companyId = params.company_id
   }
